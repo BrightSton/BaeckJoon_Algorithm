96a75c865a41dc9bbdde1288b233b1e80a0e43f9
LeeHuynSuk <gusehf321@naver.com>
2022-11-06 21:43:32 +0900

    [b 10951] A + B -4

---
+++ 문제 10951/app.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((item) => item.split(" "));
+
+let A = input;
+
+//console.log(A);
+
+solution(A);
+
+function solution(A) {
+  // Write your code
+  let answer = 0;
+  for (let i = 0; i < A.length; i++) {
+    answer = Number(A[i][0]) + Number(A[i][1]);
+    console.log(answer);
+  }
+}
