7a25c4518d80681f80856c934d8b326a996058ec
LeeHuynSuk <gusehf321@naver.com>
2022-11-29 18:31:20 +0900

    [b 10809] 알파벳 찾기

--- 문제 10809/app.js
+++ 문제 10809/app.js
@@ -8,7 +8,7 @@ let N = input;
 //console.log(N);
 
 //아스키 코드를 활용해서 a ~ z까지가 97번 ~ 122번 까지이기에 반복해주고
-//indexOf메서드를 통해 index값이 없다면 -1을 출력.
+//indexOf메서드를 통해 index값이 없다면 -1을 출력. (메서드 사용 중요!)
 solution(N);
 
 function solution(A) {
