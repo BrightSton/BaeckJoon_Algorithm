94243a952301f5db50855a18e801f377d16815bf
LeeHuynSuk <gusehf321@naver.com>
2022-12-01 23:58:07 +0900

    [b 1157] 단어 공부

---
+++ 문제 1157/app.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+let N = input;
+//console.log(N);
+
+solution(N);
+
+function solution(N) {
+  // Write your code
+  let lowN = N.toLowerCase().split("");
+  let word = {};
+  lowN.forEach((e) => {
+    word[e] = (word[e] || 0) + 1;
+  });
+  let result = "";
+  let count = 0;
+  for (a in word) {
+    if (word[a] > count) {
+      count = word[a];
+      result = a.toUpperCase();
+    } else if (word[a] === count) {
+      result = "?";
+    }
+  }
+  console.log(result);
+}
